8f452c372b5520f34b3a2c1c9c60830432931b10
ciittraininginstitute <ciitinstitutepune@gmail.com>
2023-05-27 12:12:20 -0700

    sdfsdf

---
+++ MVCProjectForGit/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProjectForGit.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
